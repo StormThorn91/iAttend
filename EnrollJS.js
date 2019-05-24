666b239d20a175c66020c6aef29b814912d3660c
StormThorn91 <caitlinsistoso@gmail.com>
2019-05-24 15:12:27 +0800

    Add files via upload

---
+++ EnrollJS.js
@@ -0,0 +1,34 @@
+function configFirebase(){
+	var firebaseConfig = {
+    	apiKey: "AIzaSyACCuOwFZCR_-pjj53puO5YmymKM3pdngs",
+    	authDomain: "iattend-4b494.firebaseapp.com",
+    	databaseURL: "https://iattend-4b494.firebaseio.com",
+    	projectId: "iattend-4b494",
+    	storageBucket: "iattend-4b494.appspot.com",
+    	messagingSenderId: "37193368388",
+    	appId: "1:37193368388:web:e2617c9ea6f6dfa0"
+  };
+  		// Initialize Firebase
+  		firebase.initializeApp(firebaseConfig);
+}
+
+function getData(){
+	var SID = document.getElementById("SID").value
+	var CID = document.getElementById("CID").value
+	var CYS = document.getElementById("CYS").value
+	var fname = document.getElementById("FirstName").value
+	var mname = document.getElementById("MiddleName").value
+	var lname = document.getElementById("LastName").value
+	writeToDB(SID, CID, CYS, fname, mname, lname)
+
+}
+
+function writeToDB(SID, CID, CYS, fname,mname, lname){
+	var db = firebase.database()
+	db.ref("Courses").child(CID.toString()).child(SID.toString()).set({
+		firstname: fname,
+		middlename: mname,
+		lastname: lname,
+		CYS: CYS
+	})
+}
